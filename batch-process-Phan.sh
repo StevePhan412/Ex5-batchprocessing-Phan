de8cdc5cfdcc7030c1b609b3c9ed98d2a96869c7
StevePhan412 <43378561+StevePhan412@users.noreply.github.com>
2023-09-27 21:24:27 -0500

    source code created
    Created the source code and test successfully

--- batch-process-Phan.sh
+++ batch-process-Phan.sh
@@ -1,9 +1,33 @@
-#! /bin/sh 
-for f in "$1"/*; do
-  if [ -f "$f" ]; then
-    SIZE="$(du -sh "${f}" | cut -f1)"
-    echo "Processing $f file..."
-    echo "$SIZE"
+#!/bin/bash
+
+# Check if a directory is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 [directory]"
+  exit 1
+fi
+
+# Verify that the directory exists
+if [ ! -d "$1" ]; then
+  echo "Error: Directory '$1' does not exist."
+  exit 1
+fi
+
+# Header
+echo "File Name | File Size (Bytes) | Word Count"
+
+# Iterate over each file in the directory
+for file in "$1"/*; do
+  # Check if it is a file and not a directory
+  if [ -f "$file" ]; then
+    # Get file size
+    filesize=$(stat -c %s "$file")
+
+    # Get word count
+    wordcount=$(wc -w < "$file")
+
+    # Output information
+    echo "$file | $filesize | $wordcount"
   fi
 done
+
 
